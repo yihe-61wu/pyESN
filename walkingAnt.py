97e54f150d802c0ff409c4c4188ab219c91c37fa
yihe-piperdam <lu.yihe.61wu@gmail.com>
2022-09-20 18:20:35 +0100

    fliprot wierdly works

--- walkingAnt.py
+++ walkingAnt.py
@@ -6,19 +6,30 @@ def rotate(x, y, degrees):
     return vector * np.exp(complex(0, np.deg2rad(degrees)))
 
 
-def ant_step(current_location, last_location, current_potential, last_potential, rotataion=20, stepsize=0.05):
+def ant_step(current_location, last_location, current_potential, last_potential, rotation=90, stepsize=0.1, method='fliprot'):
     xy_curr, xy_last, p_curr, p_last = [np.array(arg) for arg in (current_location, last_location, current_potential, last_potential)]
 
     dxy_last = xy_curr - xy_last
     direction_last = dxy_last / np.linalg.norm(dxy_last)
 
     dp_last = p_curr - p_last
-    reverse = int(dp_last > 0) * 2 - 1
 
-    new_direction = rotate(*(-reverse * direction_last), rotataion)
-    direction_curr = np.array([new_direction.real, new_direction.imag])
+    if method == 'reverse':
+        reverse = int(dp_last < 0) * 2 - 1
 
-    return direction_curr * stepsize
+        new_direction = rotate(*(reverse * direction_last), rotation)
+        direction_curr = np.array([new_direction.real, new_direction.imag])
+
+    elif method == 'fliprot':
+        if dp_last > 0:
+            new_direction = rotate(*direction_last, rotation)
+        else:
+            new_direction = -rotate(*direction_last, -rotation)
+
+        direction_curr = np.array([new_direction.real, new_direction.imag]) #+ np.random.rand(2) * 0.01
+
+
+    return direction_curr * stepsize, dp_last < 0
 
 
 if __name__ == "__main__":
@@ -31,23 +42,32 @@ if __name__ == "__main__":
 
     x0 = np.random.randint(2) * 5 - 2.5
     y0 = np.random.rand() * 5 - 2.5
-    x1, y1 = x0 + .1, y0 + .1
+    init_offset = np.random.rand(2) * .2 - .1
+    x1, y1 = x0 + init_offset[0], y0 + init_offset[1]
 
     duration = 100
     xt, yt = np.zeros(duration), np.zeros(duration)
-    change = []
-    potential = []
+    degree = 20
+    pt = np.zeros(duration)
     for t in range(duration):
         xt[t], yt[t] = x1, y1
         p0, p1 = [landscape.pdf(xy) for xy in ([x0, y0], [x1, y1])]
-        s = ant_step([x1, y1], [x0, y0], p0, p1)
+        #p0, p1 = y0, y1
+        s, flip = ant_step([x1, y1], [x0, y0], p0, p1, rotation=degree)
+        if flip:
+            degree *= -1
+
         x0, y0 = x1, y1
         x1 += s[0]
         y1 += s[1]
 
-    plt.figure(figsize=(10, 10))
+        pt[t] = p1
+
+
+    print(xt[0], yt[0])
     plt.contourf(x, y, landscape.pdf(pos))
     plt.plot(xt, yt, c='r')
     plt.axis('square')
+    plt.figure()
+    plt.plot(pt)
     plt.show()
-    print(x0, y0)
